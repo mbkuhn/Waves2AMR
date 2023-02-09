5bc5902a51ae2af21a94bea8d470aab3241c9223
Michael Kuhn <mkuhn@nrel.gov>
2023-02-09 10:36:04 -0700

    test setup with small HOS data file

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,8 +1,7 @@
 #include "../src/read_modes.h"
 
 int main() {
-  std::string fname = "/Users/mkuhn/testruns_data/HOS/Results_gdeskos_eagle/"
-                      "modes_HOS_SWENSE.dat";
+  std::string fname = "../tests/modes_HOS_SWENSE.dat";
   ReadModes rmodes(fname);
 
   rmodes.print_file_constants();
