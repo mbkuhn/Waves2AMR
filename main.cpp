a5116051d36b2b25adaa0930df681837c8ded395
Michael Kuhn <mkuhn@nrel.gov>
2023-02-08 21:40:31 -0700

    initial gitignore and main -- main is really just a testing function

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include "read_modes.h"
+
+int main() {
+  std::string fname = "/Users/mkuhn/testruns_data/HOS/Results_gdeskos_eagle/"
+                      "modes_HOS_SWENSE.dat";
+  ReadModes rmodes(fname);
+
+  rmodes.print_file_constants();
+}
