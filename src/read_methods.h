c5f6b3fede68f9ad6cf5ceff20f1a66ae0e7b896
Michael Kuhn <mkuhn@nrel.gov>
2023-02-09 14:31:50 -0700

    moving more things to header files

--- src/read_methods.h
+++ src/read_methods.h
@@ -1,14 +1,21 @@
 #include "read_modes.h"
 
-void ReadModes::ascii_initialize() {
+inline void ReadModes::ascii_initialize() {
   std::stringstream fname;
   fname << m_filename;
   std::ifstream is(fname.str());
 
   double d_n1, d_n2;
   is >> d_n1 >> d_n2 >> f_out >> T_stop >> xlen >> ylen >> depth >> g >> L >> T;
+
+  // f_out is initially the dt_out
+  // xlen, ylen, depth, and g are nondimensionalized
+  // g, L, and T are dimensional
+
+  // Convert values
   n1 = (int)d_n1;
   n2 = (int)d_n2;
+  f_out = 1.0 / f_out;
 
   // Get working dimensions
   n1o2p1 = n1/2 + 1;
@@ -17,7 +24,7 @@ void ReadModes::ascii_initialize() {
   
 }
 
-void ReadModes::ascii_read(int itime) {
+inline void ReadModes::ascii_read(int itime) {
 
   /*std::copy(
     std::istream_iterator<double>(istream), 
--- src/read_modes.h
+++ src/read_modes.h
@@ -7,7 +7,7 @@
 #include <vector>
 class ReadModes {
 public:
-  ReadModes(std::string);
+  ReadModes(std::string, bool nondim);
 
   void print_file_constants();
 
@@ -18,8 +18,20 @@ public:
                    std::vector<double> &v5, std::vector<double> &v6);
 
   void get_data(double time, std::vector<double> &mX, std::vector<double> &mY,
-                 std::vector<double> &mZ, std::vector<double> &mT,
-                 std::vector<double> &mFS, std::vector<double> &mFST);
+                std::vector<double> &mZ, std::vector<double> &mT,
+                std::vector<double> &mFS, std::vector<double> &mFST);
+
+  // Output functions for testing
+  int get_n1() { return n1; }
+  int get_n2() { return n2; }
+  double get_f() { return f_out; }
+  double get_Tstop() { return T_stop; }
+  double get_xlen() { return xlen; }
+  double get_ylen() { return ylen; }
+  double get_depth() { return depth; }
+  double get_g() { return g; }
+  double get_L() { return L; }
+  double get_T() { return T; }
 
 private:
   // Convert time to timestep
@@ -28,6 +40,9 @@ private:
   // ASCII functions
   void ascii_initialize();
   void ascii_read(int itime);
+
+  // Dimensionalize read-in quantities
+  void dimensionalize();
 
   // HOS data filename
   std::string m_filename;
@@ -47,13 +62,18 @@ private:
   int itime_now;
 };
 
-inline ReadModes::ReadModes(std::string filename) : m_filename(filename) {
+inline ReadModes::ReadModes(std::string filename, bool nondim = false) : m_filename(filename) {
   // Set time index value
   itime_now = 0;
   // TODO: Determine filetype
 
   // Initialize (TODO: according to file type)
   ascii_initialize();
+
+  // Nondimensionalize
+  if (!nondim) {
+    dimensionalize();
+  }
 }
 
 inline int ReadModes::time2step(double time) {
@@ -66,16 +86,41 @@ inline int ReadModes::time2step(double time) {
   return itime_now;
 }
 
+inline void ReadModes::dimensionalize() {
+  // Dimensionalize read-in nondim quantities
+  f_out /= T;
+  T_stop *= T;
+  xlen *= L;
+  ylen *= L;
+  depth *= L;
+  g *= L/T/T;
+}
+
 inline void ReadModes::read_data(double time) {
   int itime = time2step(time);
   // Read (TODO: according to file type)
   ascii_read(itime);
 }
 
+inline void
+ReadModes::output_data(std::vector<double> &v1, std::vector<double> &v2,
+                       std::vector<double> &v3, std::vector<double> &v4,
+                       std::vector<double> &v5, std::vector<double> &v6) {
+  // Copy class variables to input/output variables
+  std::copy(modeX.begin(), modeX.end(), std::back_inserter(v1));
+  std::copy(modeY.begin(), modeY.end(), std::back_inserter(v2));
+  std::copy(modeZ.begin(), modeZ.end(), std::back_inserter(v3));
+  std::copy(modeT.begin(), modeT.end(), std::back_inserter(v4));
+  std::copy(modeFS.begin(), modeFS.end(), std::back_inserter(v5));
+  std::copy(modeFST.begin(), modeFST.end(), std::back_inserter(v6));
+}
+
 inline void ReadModes::get_data(double time, std::vector<double> &mX,
-                         std::vector<double> &mY, std::vector<double> &mZ,
-                         std::vector<double> &mT, std::vector<double> &mFS,
-                         std::vector<double> &mFST) {
+                                std::vector<double> &mY,
+                                std::vector<double> &mZ,
+                                std::vector<double> &mT,
+                                std::vector<double> &mFS,
+                                std::vector<double> &mFST) {
   // Read data
   read_data(time);
   // Copy data to output
@@ -88,18 +133,6 @@ inline void ReadModes::print_file_constants() {
   std::cout << "n1 " << n1 << " n2 " << n2 << std::endl;
   std::cout << "xlen " << xlen << " ylen " << ylen << std::endl;
   std::cout << "depth " << depth << " g " << g << " L " << L << std::endl;
-}
-
-inline void ReadModes::output_data(std::vector<double> &v1, std::vector<double> &v2,
-                            std::vector<double> &v3, std::vector<double> &v4,
-                            std::vector<double> &v5, std::vector<double> &v6) {
-  // Copy class variables to input/output variables
-  std::copy(modeX.begin(), modeX.end(), std::back_inserter(v1));
-  std::copy(modeY.begin(), modeY.end(), std::back_inserter(v2));
-  std::copy(modeZ.begin(), modeZ.end(), std::back_inserter(v3));
-  std::copy(modeT.begin(), modeT.end(), std::back_inserter(v4));
-  std::copy(modeFS.begin(), modeFS.end(), std::back_inserter(v5));
-  std::copy(modeFST.begin(), modeFST.end(), std::back_inserter(v6));
 }
 
 #endif
