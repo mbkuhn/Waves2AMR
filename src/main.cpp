78f1b1dda2d19f4da7a53701ebe0399464dbe19d
Michael Kuhn <mkuhn@nrel.gov>
2023-02-09 09:52:28 -0700

    removing main.cpp

--- src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "read_modes.h"
-
-int main() {
-  std::string fname = "/Users/mkuhn/testruns_data/HOS/Results_gdeskos_eagle/"
-                      "modes_HOS_SWENSE.dat";
-  ReadModes rmodes(fname);
-
-  rmodes.print_file_constants();
-}
