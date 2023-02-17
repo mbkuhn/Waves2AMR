88f06f58c50329a65f57c409e7f8621adf851aa1
Michael Kuhn <mkuhn@nrel.gov>
2023-02-17 16:58:37 -0700

    cosine test passing

--- unit_tests/test_hosgrid.cpp
+++ unit_tests/test_hosgrid.cpp
@@ -86,7 +86,7 @@ TEST_F(HOSGridTest, SineSolution) {
   fftw_destroy_plan(plan_c2r);
 
   // Check against initial result
-  double factor = 1.0 / nx / ny;
+  double factor = 0.25 / ny;
   for (int ix = 0; ix < nx; ++ix) {
     for (int iy = 0; iy < ny; ++iy) {
       EXPECT_NEAR(sin(omega0 * (double)iy / (double)ny),
@@ -155,11 +155,52 @@ TEST_F(HOSGridTest, Sine) {
   modes_hosgrid::populate_hos_eta(nx, ny, plan, ptr_modes, spatial2D);
 
   // Solution should be sine curve
-  double factor = 1.0 / nx / ny;
   for (int ix = 0; ix < nx; ++ix) {
     for (int iy = 0; iy < ny; ++iy) {
       EXPECT_NEAR(sin(omega0 * (double)ix / (double)nx),
                   spatial2D[ix * ny + iy], 1e-15);
+    }
+  }
+
+  // Delete complex pointer to allocated data
+  delete[] ptr_modes;
+  // Delete plan
+  fftw_destroy_plan(plan);
+}
+
+TEST_F(HOSGridTest, Cosine) {
+  // Create modes in vector form
+  int nx = 10;
+  int ny = 16;
+  int nf = 2;
+  double omega0 = 2.0 * M_PI * nf;
+  std::vector<std::complex<double>> modes2D;
+  modes2D.resize(nx * (ny / 2 + 1));
+  for (int ix = 0; ix < nx; ++ix) {
+    for (int iy = 0; iy < ny / 2 + 1; ++iy) {
+      (modes2D[ix * (ny / 2 + 1) + iy])
+          .real((iy == nf && ix == 0 ? 2.0 * ny : 0.0));
+      (modes2D[ix * (ny / 2 + 1) + iy]).imag(0.0);
+    }
+  }
+
+  // Set up fftw_complex ptr and get plan
+  fftw_plan plan;
+  fftw_complex *ptr_modes =
+      modes_hosgrid::allocate_plan_copy(nx, ny, plan, modes2D);
+
+  // Setup output vector
+  std::vector<double> spatial2D;
+  spatial2D.resize((nx * ny));
+  // Perform fftw
+  modes_hosgrid::populate_hos_eta(nx, ny, plan, ptr_modes, spatial2D);
+
+  // Solution should be sine curve
+  double factor = 0.25 / ny;
+  for (int ix = 0; ix < nx; ++ix) {
+    for (int iy = 0; iy < ny; ++iy) {
+      EXPECT_NEAR(cos(omega0 * (double)iy / (double)ny),
+                  factor * spatial2D[ix * ny + iy], 1e-15);
     }
   }
 
