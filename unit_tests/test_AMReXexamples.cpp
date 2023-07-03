66d1a9cb86ceef7a91d648ba817a132dd9a9317a
Michael Kuhn <mkuhn@nrel.gov>
2023-07-03 15:46:31 -0600

    fixing unit test for GPUs

--- unit_tests/test_AMReXexamples.cpp
+++ unit_tests/test_AMReXexamples.cpp
@@ -4,6 +4,15 @@
 
 namespace aux_tests {
 
+namespace {
+
+void do_parallel_part(amrex::Box bx, amrex::Array4<amrex::Real> data_slab) {
+  amrex::ParallelFor(bx, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
+    data_slab(i, j, k) = (amrex::Real)(i + j + k);
+  });
+}
+} // namespace
+
 class AMReXExampleTest : public testing::Test {};
 
 // Not actually a test of the code, but a working example of AMReX steps
@@ -11,9 +20,7 @@ TEST_F(AMReXExampleTest, FillFAB) {
   amrex::Box bx(amrex::IntVect{0, 0, 0}, amrex::IntVect{2, 2, 1});
   amrex::FArrayBox fab(bx, 1);
   amrex::Array4<amrex::Real> const &data_slab = fab.array();
-  amrex::ParallelFor(bx, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
-    data_slab(i, j, k) = (amrex::Real)(i + j + k);
-  });
+  do_parallel_part(bx, data_slab);
 }
 
 } // namespace aux_tests
